bda107df52f36bb48fafa9eec7e54604cf9038d1
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-01-07 17:49:11 +0330

    edit navbar, add functionality to search bar when is focused and focus out

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let hamburgerMenu = document.getElementById("hamburger-menu")
 let navbar = document.getElementById("navbar")
+let searchInput = document.getElementById("search-input")
+let searchBar = document.getElementById("search-bar")
 
 function toggleNavbar(){
-
     if (navbar.classList.contains("is-close")){
         navbar.classList.remove("is-close")
-      
 
     } else {
         navbar.classList.add("is-close")
     }
 }
+
+searchInput.addEventListener("focus", () => {
+    searchBar.classList.add("is-focused")
+})
+
+searchInput.addEventListener("focusout", () => {
+    searchBar.classList.remove("is-focused")
+})
+
