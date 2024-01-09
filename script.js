4d830271881735a8499b29d008efb567febea477
Fateme Noori Moghadam <fatemenm058@gmail.com>
2024-01-09 12:43:54 +0330

    add event handlers for chnaging and expanding new-note-shortcut

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let searchInput = document.getElementById("search-input")
 let searchBar = document.getElementById("search-bar")
 let buttonSearch = document.getElementById("button-search")
 let buttonCloseSearchbar = document.getElementById("button-close-searchbar")
+let newNoteWrapper = document.getElementById("new-note-wrapper")
 let newNoteShortcut = document.getElementById("new-note-shortcut")
 
 function toggleNavbar(){
@@ -29,6 +30,25 @@ buttonSearch.addEventListener("click", () => {
 
 buttonCloseSearchbar.addEventListener("click", () => {
     searchBar.classList.remove("is-focused")
+})
+
+newNoteWrapper.querySelector("#new-note-shortcut").querySelector("input").addEventListener("focus", () => {
+    newNoteWrapper.classList.remove("is-view")
+    newNoteWrapper.classList.add("is-edit")
+    newNoteWrapper.querySelector("#new-note-shortcut-expanded").querySelector("#new-note-body").focus()
+})
+
+
+newNoteWrapper.querySelector("#new-note-shortcut-expanded").querySelector("#new-note-body").addEventListener("focusout", () => {
+    newNoteWrapper.querySelector("#new-note-shortcut").querySelector("input").focus()
+    newNoteWrapper.classList.remove("is-edit")
+    newNoteWrapper.classList.add("is-view") 
+})
+
+newNoteWrapper.querySelector("#new-note-shortcut-expanded").querySelector("#button-close").addEventListener("click", () => {
+    newNoteWrapper.querySelector("#new-note-shortcut").querySelector("input").focus()
+    newNoteWrapper.classList.remove("is-edit")
+    newNoteWrapper.classList.add("is-view") 
 })
 
 
